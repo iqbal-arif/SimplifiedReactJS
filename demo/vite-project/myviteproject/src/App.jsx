bddce8841cfb712354c28fb5b39af84114d77ef6
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-08-17 11:52:44 +0500

    modified:   demo/vite-project/myviteproject/src/App.jsx

--- demo/vite-project/myviteproject/src/App.jsx
+++ demo/vite-project/myviteproject/src/App.jsx
@@ -1,5 +1,7 @@
 function App() {
-  return "Hello World";
+  return React.createElement("h1", { id: "5" }, "Hello World");
+  // Both, above and below statment writes same output.
+  // return <h1 id="5">Hello World</h1>;
 }
 
 export default App;
