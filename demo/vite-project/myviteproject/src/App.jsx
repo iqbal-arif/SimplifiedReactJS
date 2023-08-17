469429ca45dc148b27b995bd4e4744cdf3c73fb9
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-08-17 12:14:16 +0500

    modified:   demo/vite-project/myviteproject/src/App.jsx

--- demo/vite-project/myviteproject/src/App.jsx
+++ demo/vite-project/myviteproject/src/App.jsx
@@ -2,6 +2,17 @@ function App() {
   return React.createElement("h1", { id: "5" }, "Hello World");
   // Both, above and below statment writes same output.
   // return <h1 id="5">Hello World</h1>;
+  // div with the class large and the id largeDiv with the text Hi
+  // Remove the text hi
+  // Add a label with the for prop set to inpuId adn any text
+  // Add an input with the id inputId, type number, and value of 3 as a number.
+
+  return (
+    <div className="large" id="largeDiv">
+      <label htmlFor="inputId">First label</label>
+      <input id="inputId" type="number" value={3} />
+    </div>
+  );
 }
 
 export default App;
