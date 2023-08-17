bf66133c4d4112dcc23a9d7781897ef5d8805218
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-08-17 12:17:31 +0500

    modified:   demo/vite-project/myviteproject/src/App.jsx

--- demo/vite-project/myviteproject/src/App.jsx
+++ demo/vite-project/myviteproject/src/App.jsx
@@ -6,11 +6,16 @@ function App() {
   // Remove the text hi
   // Add a label with the for prop set to inpuId adn any text
   // Add an input with the id inputId, type number, and value of 3 as a number.
-
+  // To be able to allow the default value 3 use defaultValue={3} instead of value={3}
+  
   return (
     <div className="large" id="largeDiv">
       <label htmlFor="inputId">First label</label>
+      {/* Does not change value 3 */}
       <input id="inputId" type="number" value={3} />
+      {/* Allows to change value 3 to any number */}
+        input id="inputId" type="number" defaultValue={3} />
+    </div>
     </div>
   );
 }
