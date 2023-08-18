6559e229049b2f12f823cbe6b43ae5ec3d30b849
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-08-18 20:53:25 +0500

    modified:   Concepts/2. Basic Stateful Components/1.useState Hook.js

--- Concepts/2. Basic Stateful Components/1.useState Hook.js
+++ Concepts/2. Basic Stateful Components/1.useState Hook.js
@@ -1,12 +1,17 @@
 import { useState } from "react";
 
 function App() {
-  const [name, setName] = useState();
+  const [name, setName] = useState("Arif");
+  console.log("Render", name);
   //useState sets value as an Array
   /*
     const name = values[0] // first value of array
     const setName = values[1] // Second value of array
     */
-  return <h1>Hi {name}</h1>;
+  function handleClick() {
+    setName("Iqbal");
+  }
+  // On click the entier component is re-render on state variable change
+  return <h1 onClick={handleClick}>Hi {name}</h1>;
 }
 export default App;
