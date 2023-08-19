abfa67d0d8b6a892245ca456a2350e3763142646
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-08-20 01:16:35 +0500

    modified:   demo/vite-project/myviteproject/src/Counter.jsx

--- demo/vite-project/myviteproject/src/Counter.jsx
+++ demo/vite-project/myviteproject/src/Counter.jsx
@@ -6,5 +6,12 @@ export function Counter() {
   function handleClick() {
     setCount(count + 1);
   }
-  return <h2 onClick={handleClick}>Counter {count}</h2>;
+  //   return <h2 onClick={handleClick}>Counter {count}</h2>;
+
+  //Condensed from of the above is
+  return (
+    <div onClick={() => setCount((currentCount) => currentCount + 1)}>
+      Counter {count}
+    </div>
+  );
 }
