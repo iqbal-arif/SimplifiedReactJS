70e718ba76aae285e96784451a2e2b17858e8fa3
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-08-20 01:17:59 +0500

    modified:   demo/vite-project/myviteproject/src/Counter.jsx

--- demo/vite-project/myviteproject/src/Counter.jsx
+++ demo/vite-project/myviteproject/src/Counter.jsx
@@ -3,9 +3,9 @@ import { useState } from "react";
 export function Counter() {
   const [count, setCount] = useState(0); // change the state.
 
-  function handleClick() {
-    setCount(count + 1);
-  }
+  //   function handleClick() {
+  //     setCount(count + 1);
+  //   }
   //   return <h2 onClick={handleClick}>Counter {count}</h2>;
 
   //Condensed from of the above is
