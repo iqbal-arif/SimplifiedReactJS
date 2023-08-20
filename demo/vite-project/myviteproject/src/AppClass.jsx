119b23867e6467b36471e7c1e12862aa4b313df0
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-08-20 23:39:06 +0500

    modified:   demo/vite-project/myviteproject/src/AppClass.jsx

--- demo/vite-project/myviteproject/src/AppClass.jsx
+++ demo/vite-project/myviteproject/src/AppClass.jsx
@@ -1,5 +1,14 @@
-export default class App {
+import React from "react";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "You are Rendering Class Component",
+    };
+  }
   render() {
-    return <h1>Hello App Class Component</h1>;
+    return <h1>Hello {this.state.name}</h1>;
   }
 }
