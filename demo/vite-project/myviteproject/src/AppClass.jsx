f09c1318572ad550b2446f930d2ae5e36678a6f2
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-08-20 23:52:24 +0500

    modified:   demo/vite-project/myviteproject/src/AppClass.jsx

--- demo/vite-project/myviteproject/src/AppClass.jsx
+++ demo/vite-project/myviteproject/src/AppClass.jsx
@@ -9,7 +9,15 @@ export default class App extends React.Component {
     };
   }
   render() {
+    //Defining onClick as a separate function
+    /*
+    const handleClick = () =>{
+        this.setState({ name: "Modifying AppClass State" })
+    }*/
+    // replace the inline arrow function with onClick={handleClick}
+
     return (
+      //Defining onClick arrow function as a line function.
       <h1 onClick={() => this.setState({ name: "Modifying AppClass State" })}>
         Hello {this.state.name}
       </h1>
