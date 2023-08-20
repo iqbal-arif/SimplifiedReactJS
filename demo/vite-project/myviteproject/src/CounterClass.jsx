3097763e4c01ceb9c33cc38cfd0c1d2202b67982
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-08-21 01:06:48 +0500

    modified:   demo/vite-project/myviteproject/src/CounterClass.jsx

--- demo/vite-project/myviteproject/src/CounterClass.jsx
+++ demo/vite-project/myviteproject/src/CounterClass.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
 
-export default class Counter extends React.Component {
+export default class CounterClass extends React.Component {
   constructor(props) {
     super(props);
 
