550e086145af409ec30e5bd060dba62d2b5053e0
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-08-21 12:38:26 +0500

    modified:   demo/vite-project/myviteproject/src/Input.jsx

--- demo/vite-project/myviteproject/src/Input.jsx
+++ demo/vite-project/myviteproject/src/Input.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { useState } from "react";
 
-export default function Input() {
+function Input() {
   const { name, setName } = useState("Initial Value of Input");
 
   return (
     <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
   );
 }
+
+export default Input;
