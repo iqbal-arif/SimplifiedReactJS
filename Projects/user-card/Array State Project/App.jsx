94e3a0e32667a2a013fe5ff502b7c0d36714c2d8
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-08-23 00:03:13 +0500

    modified:   Projects/user-card/Array State Project/App.jsx

--- Projects/user-card/Array State Project/App.jsx
+++ Projects/user-card/Array State Project/App.jsx
@@ -2,4 +2,15 @@ import { useState } from "react";
 
 function App() {
   const [array, setArray] = useState(["A", "B", "C"]);
+
+  return (
+    <>
+      <button onClick={removeFirstElement}>Remove First Element</button>
+      <br />
+      <br />
+      {array.join(",")}
+    </>
+  );
 }
+
+export default App;
