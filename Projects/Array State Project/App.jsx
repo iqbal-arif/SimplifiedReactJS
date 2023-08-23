1d8b6a130816157c2a2f904e0352ebb5d532e2a2
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-08-23 23:37:43 +0500

    modified:   Projects/Array State Project/App.jsx

--- Projects/Array State Project/App.jsx
+++ Projects/Array State Project/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 function App() {
+  /*1. Create state that stores an array with the initial value of ["A", "B", "C"]*/
   const [array, setArray] = useState(["A", "B", "C"]);
 
   return (
