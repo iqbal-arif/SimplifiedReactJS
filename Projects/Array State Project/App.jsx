9c1b8d25a60e3b3c8027d4ad4b4037f482be32d9
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-08-23 23:42:32 +0500

    modified:   Projects/Array State Project/App.jsx

--- Projects/Array State Project/App.jsx
+++ Projects/Array State Project/App.jsx
@@ -3,7 +3,13 @@ import { useState } from "react";
 function App() {
   /*1. Create state that stores an array with the initial value of ["A", "B", "C"]*/
   const [array, setArray] = useState(["A", "B", "C"]);
-
+  /*2.  
+    Changing new Value for the state but not chainging the Array value*/
+  function removeFirstElement() {
+    setArray((currentArray) => {
+      return currentArray.slice(1);
+    });
+  }
   return (
     <>
       <button onClick={removeFirstElement}>Remove First Element</button>
