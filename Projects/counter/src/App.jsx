594cd680f4a4c7f7a3dcbf7a7045349986875567
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-08-24 23:23:14 +0500

    modified:   Projects/counter/src/App.jsx

--- Projects/counter/src/App.jsx
+++ Projects/counter/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 
 function App() {
-  // const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
+  const [value, setValue] = useState("");
 
   return (
     <>
       <h1>This is A Counter App</h1>
+      <input type="text" value={value} />
       <br />
       <button>-</button>
       <span>0</span>
