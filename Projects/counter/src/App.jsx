7d9ad8d793c8ce9f64f7e66e7f5f9d7a4a6ce749
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-08-24 23:09:38 +0500

    modified:   Projects/counter/src/App.jsx

--- Projects/counter/src/App.jsx
+++ Projects/counter/src/App.jsx
@@ -3,7 +3,11 @@ import { useState } from "react";
 function App() {
   // const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <h1>This is A Counter App</h1>
+    </>
+  );
 }
 
 export default App;
