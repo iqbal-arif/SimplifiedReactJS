2927c583db4b46371a934a98be28ca496419ea2a
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-08-25 20:17:03 +0500

    modified:   Projects/counter/src/App.jsx Projects/counter/src/CounterComponent.jsx

--- Projects/counter/src/App.jsx
+++ Projects/counter/src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from "react";
+// import { useState } from "react";
+import CounterComponent from "./CounterComponent";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [value, setValue] = useState("Arif");
-  // const [name, setName] = useState("");
-
-  // Name Funtion
-  function nameSet(name) {
-    return setValue({ name });
-  }
-
-  return (
-    <>
-      <h1>This is A Counter App</h1>
-      <input value={value} onChange={(e) => setValue(e.target.value)} />
-      <br />
-      <br />
-      <button onClick={() => setCount((currentCount) => currentCount - 1)}>
-        -
-      </button>
-      <span>0</span>
-      <button onClick={() => setCount((currentCount) => currentCount + 1)}>
-        +
-      </button>
-      <br />
-      <br />
-      <p>
-        My name is <h3>{value}</h3> and I am <h3>{count}</h3> years old.
-      </p>
-    </>
-  );
+  return <CounterComponent />;
 }
+// function App() {
+//   const [count, setCount] = useState(0);
+//   const [value, setValue] = useState("Arif");
+//   // const [name, setName] = useState("");
+
+//   // Name Funtion
+//   function nameSet(name) {
+//     return setValue({ name });
+//   }
+
+//   return (
+//     <>
+//       <h1>This is A Counter App</h1>
+//       <input value={value} onChange={(e) => setValue(e.target.value)} />
+//       <br />
+//       <br />
+//       <button onClick={() => setCount((currentCount) => currentCount - 1)}>
+//         -
+//       </button>
+//       <span>0</span>
+//       <button onClick={() => setCount((currentCount) => currentCount + 1)}>
+//         +
+//       </button>
+//       <br />
+//       <br />
+//       <p>
+//         My name is <h3>{value}</h3> and I am <h3>{count}</h3> years old.
+//       </p>
+//     </>
+//   );
+// }
 
 export default App;
