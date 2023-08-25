1a2bd1c397030a7f09f2f08e1df55dcc8e30751f
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-08-25 23:50:33 +0500

    modified:   demo/vite-project/Component Lifecyle/lifecycle/src/App.jsx

--- demo/vite-project/Component Lifecyle/lifecycle/src/App.jsx
+++ demo/vite-project/Component Lifecyle/lifecycle/src/App.jsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
 import Child from "./Child";
 function App() {
+  const [count, setCount] = useState(0);
+
   console.log("Rendering Parent");
   return (
     <>
+      <button
+        style={{ display: "block", marginBottom: "1rem" }}
+        onClick={() => setCount((c) => c + 1)}
+      >
+        {count}
+      </button>
       <Child />
     </>
   );
