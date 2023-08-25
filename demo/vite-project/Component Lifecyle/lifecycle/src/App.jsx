50e5ed36b20e4b6c14a46cf713ece13b81e58ea1
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-08-25 23:57:39 +0500

    modified:   demo/vite-project/Component Lifecyle/lifecycle/src/App.jsx

--- demo/vite-project/Component Lifecyle/lifecycle/src/App.jsx
+++ demo/vite-project/Component Lifecyle/lifecycle/src/App.jsx
@@ -2,10 +2,19 @@ import { useState } from "react";
 import Child from "./Child";
 function App() {
   const [count, setCount] = useState(0);
+  const [isShown, setIsShown] = useState(true);
 
   console.log("Rendering Parent");
   return (
     <>
+      <button
+        style={{ display: "block", marginBottom: "1rem" }}
+        onClick={() => setCount((c) => c + 1)}
+      >
+        {count}
+      </button>
+      <br />
+      <br />
       <button
         style={{ display: "block", marginBottom: "1rem" }}
         onClick={() => setCount((c) => c + 1)}
