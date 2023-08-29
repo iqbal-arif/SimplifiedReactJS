41eaac0374d151d812497d699062efa59e919db8
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-08-29 16:08:42 +0500

    modified:   Drills/useEffectHook/src/Child.jsx modified:   demo/vite-project/Component Lifecyle/lifecycle/src/main.jsx

--- Drills/useEffectHook/src/Child.jsx
+++ Drills/useEffectHook/src/Child.jsx
@@ -1,20 +1,34 @@
-import { useState } from "react"
+import { useState, useEffect } from "react";
 
 export function Child() {
-  const [age, setAge] = useState(0)
-  const [name, setName] = useState("")
+  const [age, setAge] = useState(0);
+  const [name, setName] = useState("");
+  console.log("Render");
 
+  //Runs once when component is mounted
+  useEffect(() => {
+    console.log("Hi");
+  }, []);
+
+  //Runs every time when name or age is changed
+  useEffect(() => {
+    console.log(`My name is ${name} and I am ${age} years old`);
+  }, [name, age]);
   return (
     <div>
-      <input type="text" value={name} onChange={e => setName(e.target.value)} />
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
       <br />
       <br />
-      <button onClick={() => setAge(a => a - 1)}>-</button>
+      <button onClick={() => setAge((a) => a - 1)}>-</button>
       {age}
-      <button onClick={() => setAge(a => a + 1)}>+</button>
+      <button onClick={() => setAge((a) => a + 1)}>+</button>
       <br />
       <br />
       My name is {name} and I am {age} years old.
     </div>
-  )
+  );
 }
