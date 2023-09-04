fa88a77d385b90cba9a19a3bc599ca6149872e9d
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-09-04 12:16:03 +0500

    modified:   Drills/classLifecycle/src/Child.jsx

--- Drills/classLifecycle/src/Child.jsx
+++ Drills/classLifecycle/src/Child.jsx
@@ -1,28 +1,32 @@
-import React from "react"
+import React from "react";
 
 export class Child extends React.Component {
   constructor() {
-    super()
+    super();
     this.state = {
       age: 0,
       name: "",
-    }
+    };
   }
 
+  componentDidUpdate() {
+    //1. `console.log` the text **Render** each time the component re-renders
+    console.log("Renders");
+  }
   render() {
     return (
       <div>
         <input
           type="text"
           value={this.state.name}
-          onChange={e => this.setState({ name: e.target.value })}
+          onChange={(e) => this.setState({ name: e.target.value })}
         />
         <br />
         <br />
         <button
           onClick={() =>
-            this.setState(state => {
-              return { age: state.age - 1 }
+            this.setState((state) => {
+              return { age: state.age - 1 };
             })
           }
         >
@@ -31,8 +35,8 @@ export class Child extends React.Component {
         {this.state.age}
         <button
           onClick={() =>
-            this.setState(state => {
-              return { age: state.age + 1 }
+            this.setState((state) => {
+              return { age: state.age + 1 };
             })
           }
         >
@@ -42,6 +46,6 @@ export class Child extends React.Component {
         <br />
         My name is {this.state.name} and I am {this.state.age} years old.
       </div>
-    )
+    );
   }
 }
