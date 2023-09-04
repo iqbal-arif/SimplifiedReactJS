b97a484b30a5841cde3b29e14bfd52d2f00a5972
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-09-05 00:02:21 +0500

    modified:   demo/vite-project/fetchAPI/src/App.jsx

--- demo/vite-project/fetchAPI/src/App.jsx
+++ demo/vite-project/fetchAPI/src/App.jsx
@@ -3,13 +3,19 @@ import { useEffect, useState } from "react";
 function App() {
   //Setting users in useState
   const [users, setUsers] = useState("");
+  //Defining Loading state when data is being fetched
+  const [loading, setLoading] = useState(true);
   //Using useEffect so the fetch runs once instead of multiple fetch
   //without useEffet it fetches multiple times.
   useEffect(() => {
+    setLoading(true);
     {
       fetch("https://jsonplaceholder.typicode.com/users")
         .then((response) => response.json())
-        .then((json) => setUsers(json));
+        .then((json) => setUsers(json))
+        .finally(() => {
+          setLoading(false);
+        });
     }
   }, []);
   console.log(users);
