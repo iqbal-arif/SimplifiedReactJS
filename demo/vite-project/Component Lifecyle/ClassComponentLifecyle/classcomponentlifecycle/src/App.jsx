b3accb116d0bdacf9c8e804a5fc85c2cbad82f19
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-09-04 11:54:02 +0500

    modified:   demo/vite-project/Component Lifecyle/ClassComponentLifecyle/classcomponentlifecycle/src/App.jsx

--- demo/vite-project/Component Lifecyle/ClassComponentLifecyle/classcomponentlifecycle/src/App.jsx
+++ demo/vite-project/Component Lifecyle/ClassComponentLifecyle/classcomponentlifecycle/src/App.jsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
+
 import { Child } from "./Child";
 
 function App() {
+  const [show, setShow] = useState(true);
+
+  const childComponent = show ? <Child /> : null;
   return (
     <>
       <p className="class component lifecycle">Class Component Lifecycle</p>
-      <Child />
+      <div>
+        <button onClick={() => setShow((currentShow) => !currentShow)}>
+          Show/Hide
+        </button>
+        {childComponent}
+      </div>
     </>
   );
 }
