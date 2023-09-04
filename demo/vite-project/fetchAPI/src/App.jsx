74830626e27bc6bc889d3f7268a9bebd8d998c29
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-09-04 23:58:51 +0500

    modified:   demo/vite-project/fetchAPI/src/App.jsx

--- demo/vite-project/fetchAPI/src/App.jsx
+++ demo/vite-project/fetchAPI/src/App.jsx
@@ -1,16 +1,18 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  //Setting users in useState
+  const [users, setUsers] = useState("");
   //Using useEffect so the fetch runs once instead of multiple fetch
   //without useEffet it fetches multiple times.
   useEffect(() => {
     {
       fetch("https://jsonplaceholder.typicode.com/users")
         .then((response) => response.json())
-        .then((json) => console.log(json));
+        .then((json) => setUsers(json));
     }
   }, []);
+  console.log(users);
   return (
     <>
       <h2>User API Fetching</h2>
