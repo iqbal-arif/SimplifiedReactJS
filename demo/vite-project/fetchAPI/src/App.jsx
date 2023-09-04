6d165c94842636d12b32c0574e9054da701e4b48
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-09-04 23:53:50 +0500

    modified:   demo/vite-project/fetchAPI/src/App.jsx modified:   demo/vite-project/fetchAPI/src/main.jsx

--- demo/vite-project/fetchAPI/src/App.jsx
+++ demo/vite-project/fetchAPI/src/App.jsx
@@ -1,11 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
-
+  //Using useEffect so the fetch runs once instead of multiple fetch
+  //without useEffet it fetches multiple times.
+  useEffect(() => {
+    {
+      fetch("https://jsonplaceholder.typicode.com/users")
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+    }
+  }, []);
   return (
     <>
-      <H2>User API Fetching</H2>
+      <h2>User API Fetching</h2>
     </>
   );
 }
