3439a46ab8d524bcccefe722cae88c587f1b26ef
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-09-04 23:38:18 +0500

    modified:   demo/vite-project/fetchAPI/src/App.jsx

--- demo/vite-project/fetchAPI/src/App.jsx
+++ demo/vite-project/fetchAPI/src/App.jsx
@@ -3,7 +3,11 @@ import { useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <H2>User API Fetching</H2>
+    </>
+  );
 }
 
 export default App;
