f7eb639463a53281781cfe3493363f64c099593e
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-09-05 23:38:09 +0500

    modified:   demo/vite-project/Advanced Components/reactDevTools/src/ClassComponent.jsx

--- demo/vite-project/Advanced Components/reactDevTools/src/ClassComponent.jsx
+++ demo/vite-project/Advanced Components/reactDevTools/src/ClassComponent.jsx
@@ -1,3 +1,28 @@
-export default function ClassComponent() {
-  return <h3>This is a Class Component</h3>;
+import React from "react";
+export class ClassComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      age: 0,
+    };
+  }
+
+  componentDidUpdate() {
+    document.title = this.state.name;
+  }
+
+  render() {
+    return (
+      <>
+        <h3>This is a Class Component</h3>
+        <input
+          value={this.state.name}
+          onChange={(e) => this.setState({ name: e.target.value })}
+        ></input>
+        <br />
+        <br />
+      </>
+    );
+  }
 }
