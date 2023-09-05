404534ec48c77ab681f96744e67e5bb2e1fac3e0
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-09-05 23:03:08 +0500

    modified:   demo/vite-project/Advanced Components/reactDevTools/src/FunctionCompnent.jsx 	modified:   demo/vite-project/Advanced Components/reactDevTools/src/main.jsx

--- demo/vite-project/Advanced Components/reactDevTools/src/FunctionCompnent.jsx
+++ demo/vite-project/Advanced Components/reactDevTools/src/FunctionCompnent.jsx
@@ -1,3 +1,26 @@
+import { useState, useeEffect } from "react";
+import { DisplayString } from "./DisplayString";
+
 export default function FunctionComponent() {
-  return <h3>This is a Function Component</h3>;
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+
+  useeEffect(() => {
+    document.title = name;
+  }, [name]);
+
+  return (
+    <>
+      <h3>This is a Function Component</h3>
+      <input
+        value={name}
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      />
+      <button onClick={() => setAge((currentAge) => currentAge - 1)}>-</button>
+      {age}
+      <button onClick={() => setAge((currentAge) => currentAge + 1)}>+</button>
+    </>
+  );
 }
