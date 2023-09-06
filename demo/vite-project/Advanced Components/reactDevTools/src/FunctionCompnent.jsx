d1a2795459f8993bf08f88887b7ff1f6f34a3465
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-09-06 23:37:12 +0500

    modified:   demo/vite-project/Advanced Components/reactDevTools/src/FunctionCompnent.jsx

--- demo/vite-project/Advanced Components/reactDevTools/src/FunctionCompnent.jsx
+++ demo/vite-project/Advanced Components/reactDevTools/src/FunctionCompnent.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import { DisplayString } from "./DisplayString";
 
 export default function FunctionComponent() {
   const [name, setName] = useState("");
