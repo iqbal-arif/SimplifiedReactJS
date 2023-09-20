9ae95073791c80b6745e5754a2faba11f097e82c
Arif Iqbal <arif_iqbal@yahoo.com>
2023-09-20 11:46:43 -0700

    modified:   Projects/user-list/src/App.jsx

--- Projects/user-list/src/App.jsx
+++ Projects/user-list/src/App.jsx
@@ -81,8 +81,19 @@ function App() {
 
 
     </>
+    {/* Instructor Method */}
+    <>
+    <h1>User List</h1>
+    <ul>
+      {users.map(user =>{
+        return <User name = {user.name} />
+      })}
+    </ul>
+    </>
 
     </div>
+
+    
   );
 }
 export default App;
