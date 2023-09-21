5fe7ee79dd09f40f339d974b77ff70ee99830137
Arif Iqbal <arif_iqbal@yahoo.com>
2023-09-21 04:51:46 -0700

    modified:   demo/vite-project/Advanced Components/spreadProps/src/App.jsx

--- demo/vite-project/Advanced Components/spreadProps/src/App.jsx
+++ demo/vite-project/Advanced Components/spreadProps/src/App.jsx
@@ -1,6 +1,11 @@
 import { useEffect, useState } from "react"
 import { User } from "./User"
 
+// Custom RAW HTML
+const CUSTOM_HTML =
+`<h1>Hi</h1>
+<div>This is React RAW HTML injection Example</div>`
+
 function App() {
   const [isLoading, setIsLoading] = useState(true)
   const [users, setUsers] = useState([])
@@ -25,6 +30,8 @@ function App() {
 
   return (
     <>
+    {/* Example of RAW HTML Injection */}
+    <div dangerouslySetInnerHTML={{__html:CUSTOM_HTML}}></div>
       <h1>User List</h1>
       {isLoading ? (
         <h2>Loading...</h2>
